9713704a772a1cd822d25b52fc37666247d134be
descarrecifo <102670040+descarrecifo@users.noreply.github.com>
2022-10-10 22:16:12 +0200

    first commit

---
+++ M72ex6_David_Escarre/src/m72ex6_David_Escarre.java
@@ -0,0 +1,20 @@
+
+package m72Ex6_David_Escarre;
+
+import java.util.Scanner;
+
+/*L’usuari/ària ha d’introduir números fins que introdueixi un nombre primer. 
+
+En el moment que l’usuari/ària introdueixi un nombre primer, el programa ha de 
+treure per pantalla el següent missatge: "Exacte, el número “x” és primer!" */
+	
+	
+	
+	
+public class m72ex6_David_Escarre {
+	
+	public static void main (String []) {
+		
+	}
+
+}
